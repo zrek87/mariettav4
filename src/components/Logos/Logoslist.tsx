da0c3a61bde3cb4c5872e36ec77cd1332b1f064c
zrek87 <zrek87@gmail.com>
2025-07-19 01:57:20 +0300

    nothing changed

--- src/components/Logos/Logoslist.tsx
+++ src/components/Logos/Logoslist.tsx
@@ -1,10 +1,7 @@
- 
-
 export const Logoslist = [
-    { path: '/logos/1.png', alt: '' },
-    { path: '/logos/2.png', alt: '' },
-    { path: '/logos/3.png', alt: '' },
-    { path: '/logos/4.png', alt: '' },
-    { path: '/logos/5.png', alt: '' },
-  ] as const;  
-  
+  { path: "/logos/1.png", alt: "" },
+  { path: "/logos/2.png", alt: "" },
+  { path: "/logos/3.png", alt: "" },
+  { path: "/logos/4.png", alt: "" },
+  { path: "/logos/5.png", alt: "" },
+] as const;
